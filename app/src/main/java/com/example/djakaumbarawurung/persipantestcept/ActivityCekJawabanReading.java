82ce621faf0a3d8b0ae8b6e9f1dfceba346051a4
ardhiesta <ardhi.mail@gmail.com>
2016-12-13 23:22:25 +0700

    hasil kuis reading

--- app/src/main/java/com/example/djakaumbarawurung/persipantestcept/ActivityCekJawabanReading.java
+++ app/src/main/java/com/example/djakaumbarawurung/persipantestcept/ActivityCekJawabanReading.java
@@ -1,20 +1,25 @@
 package com.example.djakaumbarawurung.persipantestcept;
 
+import android.content.Intent;
+import android.support.annotation.IntegerRes;
 import android.support.annotation.RequiresPermission;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.djakaumbarawurung.persipantestcept.Model_Set_get.ReadingLog;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ActivityCekJawabanReading extends AppCompatActivity {
     TextView tvNarasiCekReading, tvSoalCekReading,
             tvJawabanUserCekReading, tvKunciCekReading, tvPenjelasanCekReading;
     int indexAktivitasUser = 0;
     ArrayList<ReadingLog> aktivitasUserDiKuisReading;
+    Button btnNextReading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +31,7 @@ public class ActivityCekJawabanReading extends AppCompatActivity {
         tvJawabanUserCekReading = (TextView) findViewById(R.id.tvJawabanUserCekReading);
         tvKunciCekReading = (TextView) findViewById(R.id.tvKunciCekReading);
         tvPenjelasanCekReading = (TextView) findViewById(R.id.tvPenjelasanCekReading);
+        btnNextReading = (Button) findViewById(R.id.btnNextCekReading);
 
         aktivitasUserDiKuisReading = getIntent().getParcelableArrayListExtra("aktivitasUser");
 
@@ -33,10 +39,31 @@ public class ActivityCekJawabanReading extends AppCompatActivity {
     }
 
     public void tampilkanNextAktivitasUserReading(View view){
-        if (indexAktivitasUser < aktivitasUserDiKuisReading.size() - 1){
-            indexAktivitasUser++;
-            tampilkanAktivitasUserReading(indexAktivitasUser);
+        if (btnNextReading.getText().toString().equalsIgnoreCase("next")) {
+            if (indexAktivitasUser < aktivitasUserDiKuisReading.size() - 1) {
+                indexAktivitasUser++;
+                tampilkanAktivitasUserReading(indexAktivitasUser);
+            } else {
+                btnNextReading.setText("SHOW RESULT");
+            }
+        } else {
+            Intent intent = new Intent(ActivityCekJawabanReading.this, ActivityHasilKuis.class);
+            int jmlBenar = hitungReadingBenar();
+            intent.putExtra("benar", jmlBenar);
+            intent.putExtra("salah", aktivitasUserDiKuisReading.size() - jmlBenar);
+            startActivity(intent);
         }
+    }
+
+    private int hitungReadingBenar(){
+        int jmlBenar = 0;
+        for (int i = 0; i < aktivitasUserDiKuisReading.size(); i++){
+            if (aktivitasUserDiKuisReading.get(i).getJawabanUser()
+                    .equalsIgnoreCase(aktivitasUserDiKuisReading.get(i).getKunci())){
+                jmlBenar++;
+            }
+        }
+        return jmlBenar;
     }
 
     public void tampilkanBackAktivitasUserReading(View view){
---
+++ app/src/main/java/com/example/djakaumbarawurung/persipantestcept/ActivityHasilKuis.java
@@ -0,0 +1,23 @@
+package com.example.djakaumbarawurung.persipantestcept;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ActivityHasilKuis extends AppCompatActivity {
+    TextView tvBenar, tvSalah;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hasil_kuis);
+
+        tvBenar = (TextView) findViewById(R.id.tvJmlBenarReading);
+        tvSalah = (TextView) findViewById(R.id.tvJmlSalahReading);
+
+        int jmlBenar = getIntent().getExtras().getInt("benar");
+        int jmlsalah = getIntent().getExtras().getInt("salah");
+        tvBenar.setText(String.valueOf(jmlBenar));
+        tvSalah.setText(String.valueOf(jmlsalah));
+    }
+}
