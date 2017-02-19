4f246a6055412d2ea46778309202efa019c42d02
ardhiesta <ardhi@wonderlabs.io>
2017-02-19 09:21:31 +0700

    TambahSoalGrammar: tambah id komponen untuk question, answer, explnation

--- app/src/main/java/com/example/djakaumbarawurung/persipantestcept/manage_data/TambahSoalGrammarActivity.java
+++ app/src/main/java/com/example/djakaumbarawurung/persipantestcept/manage_data/TambahSoalGrammarActivity.java
@@ -2,15 +2,21 @@ package com.example.djakaumbarawurung.persipantestcept.manage_data;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.example.djakaumbarawurung.persipantestcept.R;
 
 /* activity untuk menambahkan soal grammar */
 public class TambahSoalGrammarActivity extends AppCompatActivity {
+    EditText etQuestion, etAnswer, etExplanation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tambah_soal_grammar);
+
+        etQuestion = (EditText) findViewById(R.id.etQuestion);
+        etAnswer = (EditText) findViewById(R.id.etAnswer);
+        etExplanation = (EditText) findViewById(R.id.etExplanation);
     }
 }
